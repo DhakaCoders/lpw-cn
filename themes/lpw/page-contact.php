a7309f396eef35c57ce7b23adbdaf3ac41118bfb
Rannojit <cserannojitsarkar22@gmail.com>
2021-11-12 22:14:16 +0600

    .

---
+++ themes/lpw/page-contact.php
@@ -0,0 +1,13 @@
+<?php
+/*Template Name: Contact*/
+get_header();
+$thisID = get_the_ID();
+?>
+<?php get_template_part('templates/page', 'banner'); ?>
+<?php get_template_part('templates/breadcrumbs'); ?>
+
+
+
+
+
+<?php get_footer(); ?>
